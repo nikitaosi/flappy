3ed7bb0e7ab83aa7d9e816bac83c2a86c8870901
Andrey <andreandr3evitch@yandex.ru>
2019-02-28 14:48:35 +0300

    Добавление спрайтов, масштабирования, фона

--- src/boilerplate/game.ts
+++ src/boilerplate/game.ts
@@ -8,11 +8,14 @@ import "phaser";
 import { MainScene } from "./scenes/mainScene";
 
 // main game configuration
+var game;
 const config: GameConfig = {
-  width: 800,
-  height: 600,
+  width: 135,
+  height: 203,
   type: Phaser.AUTO,
   parent: "game",
+  pixelArt: true,
+
   scene: MainScene,
   physics: {
     default: "arcade",
@@ -31,5 +34,26 @@ export class Game extends Phaser.Game {
 
 // when the page is loaded, create our game instance
 window.addEventListener("load", () => {
-  var game = new Game(config);
+  game = new Game(config);
+  resize();
 });
+window.addEventListener("resize", () => {
+ resize();
+});
+
+function resize() {
+  var canvas = document.querySelector("canvas");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var windowRatio = windowWidth / windowHeight;
+  var gameRatio = game.config.width / game.config.height;
+  if(windowRatio < gameRatio)
+  {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = (windowWidth / gameRatio) + "px";    }
+  else
+  {
+    canvas.style.width = (windowHeight * gameRatio) + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
---
+++ src/boilerplate/gameobjects/Player.ts
@@ -0,0 +1,25 @@
+
+
+
+
+export class Player extends Phaser.Physics.Arcade.Group {
+
+
+
+    constructor(scene,x,y,texture) {
+        super(scene,x,y,texture);
+    }
+
+    preload(): void {
+
+    }
+
+    create(): void {
+
+    }
+
+    lolkek():void
+    {
+
+    }
+}
