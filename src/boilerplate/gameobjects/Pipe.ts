e7cf18c7118aeb7e4df1090cd1fad883ef687b35
Nikita Osipov <nikitaosipov.51@gmail.com>
2019-03-02 18:36:30 +0300

    Создали и зашевелили трубу

--- src/boilerplate/gameobjects/Pipe.ts
+++ src/boilerplate/gameobjects/Pipe.ts
@@ -1,18 +1,43 @@
-export class Pipe extends Phaser.GameObjects.Group {
+export class Pipe extends Phaser.Physics.Arcade.StaticGroup {
 
-    constructor(scene,x,y) {
-        super(scene,x,y);
-        var toppipebody = this.create(113, 26, 'gs', 'pipebody.png');
-        var toppipehead = this.create(113, 46, 'gs', 'pipe.png');
-        var bottompipebody = this.create(113, 137, 'gs', 'pipebody.png');
-        var bottompipehead = this.create(113, 90, 'gs', 'pipe.png');
+    private toppipepos: integer;
+    private toppipehead: Phaser.Physics.Arcade.Sprite;
+    private toppipebody: Phaser.Physics.Arcade.Sprite;
+    private bottompipebody: Phaser.Physics.Arcade.Sprite;
+    private bottompipehead: Phaser.Physics.Arcade.Sprite;
 
-        toppipehead.flipY = true;
-        toppipebody.scaleY = 13;
-        bottompipebody.scaleY = 20;
+    constructor(scene) {
+        super(scene.physics.world, scene);
+        this.toppipepos = Phaser.Math.Between(30, 80);
+        this.toppipehead = this.create(112,  this.toppipepos, 'gs', 'pipe.png');
+        this.toppipebody = this.create(113,  this.toppipepos, 'gs', 'pipebody.png');
+        this.bottompipebody = this.create(113,  this.toppipepos+73, 'gs', 'pipebody.png');
+        this.bottompipehead = this.create(112,  this.toppipepos+73, 'gs', 'pipe.png');
+
+
+
+        this.toppipehead.flipY = true;
+        this.toppipebody.scaleY = 50;
+        this.bottompipebody.scaleY = 50;
+        this.toppipebody.setOrigin(0,1);
+        this.toppipehead.setOrigin(0,0);
+        this.bottompipebody.setOrigin(0,0);
+        this.bottompipehead.setOrigin(0,1);
+
     }
 
     preload(): void {
 
+    }
+    replacepipe(): void {
+        this.toppipepos = Phaser.Math.Between(30, 80);
+        this.toppipehead.setX(133);
+        this.toppipebody.setX(135);
+        this.bottompipehead.setX(134);
+        this.bottompipebody.setX(135);
+        this.toppipehead.setY(this.toppipepos);
+        this.toppipebody.setY(this.toppipepos);
+        this.bottompipebody.setY(this.toppipepos+73);
+        this.bottompipehead.setY(this.toppipepos+73);
     }
 }
--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -10,37 +10,40 @@ import {Grass} from "../gameobjects/Grass";
 export class MainScene extends Phaser.Scene {
 
 
-  private player: Player;
-  private pipe: Pipe;
-  private grass: Grass;
+    private player: Player;
+    private static pipe: Pipe;
+    private grass: Grass;
 
 
-  constructor() {
+    constructor() {
     super({
       key: "MainScene"
     });
-  }
+    }
 
-  preload(): void {
+    preload(): void {
     this.load.atlas('gs', './src/boilerplate/assets/gamesprites.png','./src/boilerplate/assets/gamesprites.json' );
-  }
+    }
 
-  create(): void {
+    create(): void {
 
     var background = this.add.sprite(68, 136, 'gs', 'bg.png');
-    this.pipe = new Pipe(this, 68, 136);
+    MainScene.pipe = new Pipe(this);
     this.grass = new Grass(this, 68, 163, 'gs', 'grass.png');
     this.cameras.main.setBackgroundColor('#375064');
     //this.player = new Player(this,100,100,'gs','')
-  }
+    }
 
-update(): void {
-      Phaser.Actions.IncX(this.pipe.getChildren(),-1);
-      this.pipe.children.iterate(function (pipe) {
-          var tpipe = <Phaser.Physics.Arcade.Sprite> pipe;
-          if (tpipe.x<-12) {
-              tpipe.x = 145;
-          }
-      });
-}
+    update(): void {
+//        Phaser.Actions.IncX(MainScene.pipe.getChildren(),-1);
+
+        MainScene.pipe.children.iterate(function (tpipe) {
+           var pipech = <Phaser.Physics.Arcade.Sprite> tpipe;
+           pipech.setX(pipech.x-1);
+              pipech.refreshBody();
+           if (pipech.x < -25) {
+               MainScene.pipe.replacepipe()
+           }
+        });
+    }
 }
