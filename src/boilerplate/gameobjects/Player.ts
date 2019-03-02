bcdd8bcfd71ddfc0e12040a03994ad7652cd3d96
Andrey <andreandr3evitch@yandex.ru>
2019-03-02 14:10:00 +0300

    Анимация , Гравитация и управление птичкой

--- src/boilerplate/gameobjects/Player.ts
+++ src/boilerplate/gameobjects/Player.ts
@@ -2,24 +2,18 @@
 
 
 
-export class Player extends Phaser.Physics.Arcade.Group {
+export class Player extends Phaser.Physics.Arcade.Sprite {
 
 
 
     constructor(scene,x,y,texture) {
         super(scene,x,y,texture);
-    }
 
-    preload(): void {
 
-    }
 
-    create(): void {
+
 
     }
 
-    lolkek():void
-    {
 
-    }
 }
--- src/boilerplate/scenes/mainScene.ts
+++ src/boilerplate/scenes/mainScene.ts
@@ -14,6 +14,8 @@ export class MainScene extends Phaser.Scene {
   private pipe: Pipe;
   private grass: Grass;
 
+  private player2 : Phaser.Physics.Arcade.Sprite;
+
 
   constructor() {
     super({
@@ -23,6 +25,7 @@ export class MainScene extends Phaser.Scene {
 
   preload(): void {
     this.load.atlas('gs', './src/boilerplate/assets/gamesprites.png','./src/boilerplate/assets/gamesprites.json' );
+    this.load.spritesheet('birdanim','./src/boilerplate/assets/bird.png',{ frameWidth: 17, frameHeight: 12 });
   }
 
   create(): void {
@@ -31,7 +34,33 @@ export class MainScene extends Phaser.Scene {
     this.pipe = new Pipe(this, 68, 136);
     this.grass = new Grass(this, 68, 163, 'gs', 'grass.png');
     this.cameras.main.setBackgroundColor('#375064');
-    //this.player = new Player(this,100,100,'gs','')
+   // this.player = new Player(this,100,50,'birdanim')
+  //  this.children.add(this.player);
+
+    var config = {
+      key: 'fly',
+      frames: this.anims.generateFrameNumbers('birdanim', { frames: [ 0, 1, 2 ] }),
+      frameRate: 10,
+      repeat: -1
+    };
+
+    this.anims.create(config);
+
+      this.player2 = this.physics.add.sprite(33, 135/2-5, 'birdanim');
+      this.player2.play('fly');
+
+      this.input.keyboard.on('keydown_SPACE', function (event) {
+
+          // console.log('GAME BEGIN');
+          // block.setVelocity(Phaser.Math.Between(200, 400), Phaser.Math.Between(200, 400));
+          //this.gameBegin = true;
+          this.player2.body.setVelocityY(-100);
+      },this );
+
+
+    //this.add.sprite(55, 135/2-5, 'birdanim').play('fly');
+
+
   }
 
 update(): void {
